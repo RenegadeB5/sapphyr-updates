e354f450ff7f252df1ceb6e6e5f3cea85adc510c
RenegadeB5 <rocketmanB5@gmail.com>
2018-01-31 12:45:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,25 +3,28 @@
 const Discord = require('discord.js');
 const config = require("./config.json");
 var client = new Discord.Client();
-const prefix = ".";
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-var NOTIFY_CHANNEL;
+
+client.on('ready', () => {
+    client.user.setGame("Exiled Hack! " + client.guilds.array().length + " Servers");
+    console.log('successfully Logged In As schem Bot!');
 
 client.on ('message', message => {
-  if (message.content === prefix + "spam") {
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if (command === "spam") {
         if (message.author.id === config.ownerID) {
+          let ammount = args.slice(0).join(" ");
           let text = args.slice(1).join(" ");
-          let ammount = args.slice(2);
-          for(var i=0; i < ammount; i++){
-              NOTIFY_CHANNEL.sendMessage(text, {tts: false});
+          
+              message.channel.send(text);
               
         
          
         } 
         else { 
-                NOTIFY_CHANNEL.sendMessage('Only Rene can use this command, suck a cock. ', {tts: false});
+               message.channel.send('Only Rene can use this command. ', {tts: false});
   }
   }});
  
 //LOGIN TOKEN-------------------------------------------------------------------
-client.login('MzczMTQwOTY2NjUzMDM0NTA2.DNsyfg.JlQTHYPaA7sXJ_sW4Eb7db7VWBk');
+client.login(process.env.BOT_TOKEN);
