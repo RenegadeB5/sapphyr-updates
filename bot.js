7fb5d0bd33fe7e57164632b9e1e351f740156cbb
RenegadeB5 <rocketmanB5@gmail.com>
2018-02-06 11:06:48 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = "!";
 var client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame('spambot');
+    client.user.setGame(process.env.PLAYING);
     console.log('successfully Logged In As spam bot!');
 
 client.on ('message', message => {
