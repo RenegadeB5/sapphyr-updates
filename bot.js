c492875c374dd11912f0cefa0125b343f7a2fca6
RenegadeB5 <rocketmanB5@gmail.com>
2018-02-15 11:16:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on ('message', message => {
 
 client.on ('message', message => {
   if (message.content === "!list roles") {
-    console.log(msg.channel.server.roles.map(role=>role.name););
+    console.log(msg.channel.server.roles.map(role=>role.name));
   }
 });
 
