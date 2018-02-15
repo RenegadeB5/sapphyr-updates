6ea2f851f88bfb0f649ffcde98c79faf88d13db3
RenegadeB5 <rocketmanB5@gmail.com>
2018-02-15 12:31:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on ('message', message => {
 
 client.on ('message', message => {
   if (message.content === "!list roles") {
-           var role = message.guild.roles.array
+           var role = array.from(message.guild.roles)
            console.log(role);
   }
 });
