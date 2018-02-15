4dd74c052b5b8d6f81201eb461519e0d5fcb6bb4
RenegadeB5 <rocketmanB5@gmail.com>
2018-02-15 11:42:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on ('message', message => {
 
 client.on ('message', message => {
   if (message.content === "!list roles") {
-      var roles = message.channel.server.roles.map(role=>role.name);
+      var roles = message.server.roles.map(role=>role.name);
       roles = roles.join(", ").replace(/@/g, '@\u200b');
       console.log(":black_small_square: **Roles:** `" + roles + "`");
   }
