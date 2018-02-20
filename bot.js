6b46616b7971821511272294345910c0e8623a87
RenegadeB5 <rocketmanB5@gmail.com>
2018-02-20 09:33:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,6 @@ client.on('ready', () => {
     client.user.setPresence({ game: { name: process.env.playing, type: 0 } });
     console.log('successfully Logged In As spam bot!');
     console.log(guildNames);
-    let author = 259368804293935104
-    author.sendMessage("hi")
 });
 client.on ('message', message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
