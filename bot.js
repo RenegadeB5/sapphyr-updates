55aa0df11962595e762ad4d22e1b23cca991129f
RenegadeB5 <rocketmanB5@gmail.com>
2018-09-27 10:48:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ client.on ('message', message => {
 client.on ('message', message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  if (command === "spam") {
+  if (command === "say") {
         if (message.author.id === process.env.ownerID) {
           let times = 0
           let [ammount] = args;
