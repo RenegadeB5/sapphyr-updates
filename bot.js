6da0151c8ac9cf5a1779bfeebfaf19cbf3152cd4
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-02 14:13:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,18 +46,11 @@ client.on ('message', message => {
 
 client.on('messageReactionAdd', (reaction, user) => {
     const filter = (reaction, user) => reaction.emoji.name === "🔗";
-    let collector = reaction.message.createReactionCollector(filter, { time: 7200000 });    
-    collector.on('collect', (reaction, collector) => {
-        if(reaction.emoji.name === "🔗") {
-            let dm = reaction.users.map(r => r.id);
-            let dmsend = dm[dm.length-1];
-            let party = reaction.users.map(r => r.lastMessageID);
-            let partysend = party[party.length-1];
-            console.log(reaction.message.channel.fetchMessage(partysend))
-            reaction.message.channel.fetchMessage(partysend)
-               .then(fetchedMessage => client.users.get(dmsend).send(((fetchedMessage.embeds).map(r => r.url))[0]))
-        }  
-    })
+    let collector = reaction.message.createReactionCollector(filter, { time: 7200000 });
+    collector.on('collect', (reaction, collector) => {        
+	    console.log('got a reaction');
+        });
+    
 });
 
 client.on ('message', message => {
