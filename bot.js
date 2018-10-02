05fb23a41887ca15b0bcb53e5a5258723e1ae960
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-02 14:57:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,17 +44,19 @@ client.on ('message', message => {
           }          
 }});    
 
-const filter = (reaction, user) => reaction.emoji.name === "🔗";
-let collector = reaction.message.createReactionCollector(filter, { time: 7200000 });
-collector.on('collect', (reaction, collector) => {        
+client.on('messageReactionAdd', (reaction, user) => {
+    const filter = (reaction, user) => reaction.emoji.name === "🔗";
+    let collector = reaction.message.createReactionCollector(filter, { time: 7200000 });
+    let collectlink = collector.on('collect', (reaction, collector) => {
         let dm = reaction.users.map(r => r.id);
         let dmsend = dm[dm.length-1];
         let party = reaction.users.map(r => r.lastMessageID);
         let partysend = party[party.length-1];
         client.users.get(dmsend).send(((reaction.message.embeds).map(r => r.url))[0])
-        console.log(dm);
-    });
-    
+	    console.log(dm);
+        });
+    console.log(collectlink);   
+}); 
 
 client.on ('message', message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
