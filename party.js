39bc0dd0d51719c6f2013223d446844d9c4683af
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-04 13:46:45 -0400

    Create party.js

---
+++ party.js
@@ -0,0 +1,47 @@
+var { Command } = require("discord.js-commando");
+module.exports = class PartyCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "party",
+            description: "Allows a member to post a party link of their choice.",
+            group: "basics",
+            memberName: "party"
+            args: [
+                {
+                    key: 'game',
+                    prompt: 'The name of the game.',
+                    type: 'string'
+                },
+                {
+                    key: 'gamemode',
+                    prompt: 'The gamemode of the game.',
+                    type: 'string'
+                }
+                {
+                    key: 'region',
+                    prompt: 'The region of the party invite.',
+                    type: 'string'
+                }
+                {
+                    key: 'link',
+                    prompt: 'The party invite link.(must include \"https://\")',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    async run(msg, { game, gamemode, region, link }) {
+        const linkchannel = msg.guild.channels.find("name", "member-links")
+        const embed = new Discord.RichEmbed()
+        .setColor(0x00FF00)
+        .setFooter('React with \"🔗\" to get the link.')
+        .setTitle('${game} party invite')
+        .setAuthor(msg.member.user.tag)
+        .addField("Gamemode", gamemode, true)
+        .addField("Region", region, true)
+        .setTimestamp()
+        msg.delete();
+        return linkchannel.send({embed}).then(function (msg) {msg.react('🔗')});
+    }
+};
